455b5146c7af1d739bae4fc2a8f60b5e0c87d2ce
unknown <rafikifred66@gmail.com>
2025-11-04 00:22:01 +0200

    update

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const contactRoutes = require('./routes/contacts');
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
